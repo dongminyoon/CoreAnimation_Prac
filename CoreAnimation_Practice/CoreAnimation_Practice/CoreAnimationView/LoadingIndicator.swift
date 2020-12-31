05548d1da57b9361fe5c13c2f4712d68cc231563
dongminyoon <ehdals8965@naver.com>
2021-01-01 06:48:27 +0900

    feat: 2번째: 원형 로딩 커스텀 구현

---
+++ CoreAnimation_Practice/CoreAnimation_Practice/CoreAnimationView/LoadingIndicator.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingIndicator.swift
+//  CoreAnimation_Practice
+//
+//  Created by 윤동민 on 2021/01/01.
+//
+
+import UIKit
+
+class LoadingIndicator: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
