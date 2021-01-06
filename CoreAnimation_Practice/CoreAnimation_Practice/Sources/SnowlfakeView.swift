fb8d63a20b8e40f34a6f7b93ac7bd854a688c2c5
dongminyoon <ehdals8965@naver.com>
2021-01-06 23:52:57 +0900

    feat: CAEmitterLayer 예제 - 눈오는 효과

---
+++ CoreAnimation_Practice/CoreAnimation_Practice/Sources/SnowlfakeView.swift
@@ -0,0 +1,19 @@
+//
+//  SnowlfakeView.swift
+//  CoreAnimation_Practice
+//
+//  Created by 윤동민 on 2021/01/06.
+//
+
+import UIKit
+
+class SnowlfakeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- CoreAnimation_Practice/CoreAnimation_Practice/Sources/ViewController.swift
+++ CoreAnimation_Practice/CoreAnimation_Practice/Sources/ViewController.swift
@@ -8,15 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let myCurve = CurveWave(frame: CGRect(x: 0,
-                                                 y: self.view.frame.height/2,
-                                                 width: self.view.frame.width,
-                                                 height: self.view.frame.height/2))
-        self.view.addSubview(myCurve)
     }
 }
 
